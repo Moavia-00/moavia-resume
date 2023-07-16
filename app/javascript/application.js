7ef029f9f9d364c0f06af4152b8b168942020a27
Moavia Hassan <moaviahassan7i8@gmail.com.com>
2023-07-16 13:39:58 +0500

    first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,13 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+import "./purecounter_vanilla.js"
+import "./bootstrap.bundle.min.js"
+import "./glightbox.min.js"
+import "./isotope.pkgd.min.js"
+import "./swiper-bundle.min.js"
+import "./typed.umd.js"
+import "./noframework.waypoints.js"
+import "./main.js"
+
