d3c618cb23f513bc4db461baaa4b0d36d1dacbd1
Moavia Hassan <moaviahassan7i8@gmail.com.com>
2023-09-03 21:36:09 +0500

    Signed-off-by: Moavia Hassan <moaviahassan7i8@gmail.com.com>

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Moavia
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.initialize_on_precompile = false
+    config.assets.css_compressor = nil
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Configuration for the application, engines, and railties goes here.
     #
